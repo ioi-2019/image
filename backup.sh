41c92c4f523d1c144f968c1ab1d7a39c28c92d4b
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-07-22 16:14:26 +0400

    add: backup.sh for contestant cronjob

---
+++ backup.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+# Backup script for contestant machines
+# Version 1
+#
+# Author: Ismayil Hasanov
+# Date	: 22 July 2019
+
+# Variables
+ETH0=enp2s0f1
+SRC=/home/contestant
+DST=ioibckp@172.30.20.70:/backup
+
+# Get the IP address of the machine  assuming the network adapter is called enp2s0f1
+IP4=$(/sbin/ip -o -4 addr list enp2s0f1 | awk '{print $4}' | cut -d/ -f1)
+ZIP=/tmp/$IP4.zip
+
+# Zip the home folder of the user
+sudo zip -r $ZIP $SRC
+
+# Next, rsync the zipped file to the backup server
+rsync -a -e ssh $ZIP $DST
+
+# Finally, remove the local zip file
+sudo rm $ZIP
