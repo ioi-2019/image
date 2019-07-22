5dd078128f1f783bdc86c055fe2e5080a9e7f686
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-07-22 21:25:13 +0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Backup script for contestant machines
 # Version 1
@@ -11,7 +10,7 @@ ETH0=enp2s0f1
 SRC=/home/contestant
 DST=ioibckp@172.30.20.70:/backup
 
-# Get the IP address of the machine  assuming the network adapter is called enp2s0f1
+# Get the IP address of the machine assuming the network adapter is called enp2s0f1
 IP4=$(/sbin/ip -o -4 addr list enp2s0f1 | awk '{print $4}' | cut -d/ -f1)
 ZIP=/tmp/$IP4.zip
 
@@ -20,6 +19,3 @@ sudo zip -r $ZIP $SRC
 
 # Next, rsync the zipped file to the backup server
 rsync -a -e ssh $ZIP $DST
-
-# Finally, remove the local zip file
-sudo rm $ZIP
