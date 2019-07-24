b192bf0d998289ea2e095e5e647dd3f85327e7aa
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-07-24 12:41:52 +0000

    fix: append-ip.sh done

--- dhcp/append-ip.sh
+++ dhcp/append-ip.sh
@@ -6,16 +6,41 @@
 
 # Check for the passed argument
 if [ $# -eq 0 ]; then
-    echo "Supply the CSV file, please."
-    exit 100;
+	echo "Supply the CSV file, please."
+	exit 100;
 fi
 
 # Variables
 INPUT=$1
 OUTPUT=IP_$1
+range_begin=10
+range_end=210
+host=$range_begin
+network=102
+prefix="172.30."
+max_hosts=$(( (range_end-range_begin)*2 ))
 
-# Read the CSV file
+# Check if the CSV file contains less than max_hosts machines
+mac_no=$(cat $INPUT | wc -l)
+if (( mac_no > max_hosts )); then
+	echo "Check the CSV file, it contains more addresses."
+	exit 99;
+fi
+
+# Clear the existing output file, create otherwise
+if [ -f $OUTPUT ]; then
+    rm $OUTPUT
+fi
+touch $OUTPUT
+
+# Read the CSV file and append IPs
 while read mac
 do
-	echo "$mac,IP" >> $OUTPUT
+	if (( host > range_end )); then
+		host=$range_begin
+		network=$(( network+1 ))
+	fi
+	IP=$prefix$network.$host
+	echo "$mac,$IP" >> $OUTPUT
+	host=$(( host+1 ))
 done < $INPUT
