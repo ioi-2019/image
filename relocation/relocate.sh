92d0cfb43f86abe2c60f1ced37f0df5d23179bdf
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-07-26 13:09:18 +0000

    fix: little changes

--- relocation/relocate.sh
+++ relocation/relocate.sh
@@ -9,16 +9,16 @@ set -e
 
 ### Variables
 dhcp_dir="/etc/dhcp/"
-file="reservations.conf"
+file="contestant-static.conf"
 dhcp_conf=$dhcp_dir$file
 main_csv="../../../SeatLocator/DATA_Export.csv"
 spare_laptops="laptops.csv"
 
 # Backup the conf file
-# cp $dhcp_conf ./$file.$(date +"%Y%m%d-%H%M%S")
+cp $dhcp_conf ./$file.$(date +"%Y%m%d-%H%M%S")
 
 #Test
-#dhcp_conf="mac_out.txt"
+dhcp_conf="mac_out.txt"
 
 ### Function declarations ###
 show_usage() {
@@ -26,11 +26,10 @@ show_usage() {
 	exit 1
 }
 
-# TODO: check for new
 swap_macs() {
-	# $1=old mac $2=new mac $3=file
-	sudo sed -i -e "s/$1/$2/g" -e t -e "s/$2/$1/g" $3
-	#sudo sed -i -e 's/$1/$2/g' $3
+	# $1=old_mac $2=new_mac $3=file
+	# sudo sed -i -e "s/$1/$2/g" -e t -e "s/$2/$1/g" $3
+	sudo sed -i -e "s/$1/$2/g" $3
 }
 
 ### Main program ###
@@ -76,22 +75,21 @@ fi
 printf "Old mac address: ${old_mac}\n"
 printf "New mac address: ${new_mac}\n"
 
-
-
 ### DHCP reconfigure part ###
+if [ -e $dhcp_conf ]
+then
+	# replace the old_mac with the new_mac inside DHCP conf file
+	swap_macs $old_mac $new_mac $dhcp_conf
+	printf "See the new state below:\n"
+	grep -A2 -B1 "${new_mac}" $dhcp_conf
+else
+	echo "DHCP configuration file is missing!"
+	exit 2
+fi
 
-#if [ -e $dhcp_conf ]
-#then
-#	# replace the old_mac with the new_mac inside DHCP conf file
-#	swap_macs $old_mac $new_mac $dhcp_conf
-#	printf "See the new state below:\n"
-#	grep -A2 -B1 "${old_mac}" $dhcp_conf
-#	grep -A2 -B1 "${new_mac}" $dhcp_conf
-#else
-#	echo "DHCP configuration file is missing!"
-#	exit 2
-#fi
-#
+### Change import file for seat web service ###
+
+
 #### Restart the DHCP service ###
 ## added administrator user for sudo NOPASSWD
 #printf "DHCP server is restarting...\n"
