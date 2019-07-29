e50d930401484e306961afe688a7ba16cdf04913
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-07-29 13:48:54 +0400

    fix: add numbering to scripts for order

--- isolate-install-configure.sh
+++ isolate-install-configure.sh
@@ -4,8 +4,8 @@
 # Version: 1.2
 # http://ioi2019.az/
 
-./isolate-install-configure/system-isolation.sh
-./isolate-install-configure/static_image_config.sh
-./isolate-install-configure/dynamic_image_config.sh
-./isolate-install-configure/software-installation.sh
-./isolate-install-configure/desktop-configuration.sh
+./isolate-install-configure/10-system-isolation.sh
+./isolate-install-configure/20-static-image-config.sh
+./isolate-install-configure/30-dynamic-image-config.sh
+./isolate-install-configure/40-software-installation.sh
+./isolate-install-configure/50-desktop-configuration.sh
