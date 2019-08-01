79f4f85ca18465319a4bbc166d0e3ea8600326fa
Jamal <jhasanov@gmail.com>
2019-08-01 11:02:33 +0400

    Update isolate-install-configure.sh
    Worker and contestant rule is added.

--- isolate-install-configure.sh
+++ isolate-install-configure.sh
@@ -4,10 +4,17 @@
 # Version: 1.2
 # http://ioi2019.az/
 
+# If 'contestant' is given as a parameters, all steps will be run.
+# otherwise only worker setup will be done.
+
 ./isolate-install-configure/10-system-isolation.sh
-./isolate-install-configure/20-static-image-config.sh
-./isolate-install-configure/30-dynamic-image-config.sh
-./isolate-install-configure/40-software-installation.sh
-./isolate-install-configure/50-desktop-configuration.sh
-./isolate-install-configure/60-backup-installation.sh
-./isolate-install-configure/70-zabbix-installation.sh
+./isolate-install-configure/15-wifi_bluetooth_ansible.sh
+
+if [ $1 == 'contestant' ]; then
+  ./isolate-install-configure/20-static-image-config.sh
+  ./isolate-install-configure/30-dynamic-image-config.sh
+  ./isolate-install-configure/40-software-installation.sh
+  ./isolate-install-configure/50-desktop-configuration.sh
+  ./isolate-install-configure/60-backup-installation.sh
+  ./isolate-install-configure/70-zabbix-installation.sh
+fi
