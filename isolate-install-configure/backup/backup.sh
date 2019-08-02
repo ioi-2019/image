5c47b94eb496f15112552148b68e962ec9281381
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-08-02 13:26:42 +0400

    fix: remove zip process before rsync in backup.sh

--- isolate-install-configure/backup/backup.sh
+++ isolate-install-configure/backup/backup.sh
@@ -2,19 +2,13 @@
 # Backup script for contestant machines
 #
 # Author: Ismayil Hasanov
-# Date	: 23 July 2019
+# Date	: 02 August 2019
 
 # Variables
 ETH0=enp2s0f1
-SRC=/home/contestant
-DST=ioibckp@172.30.20.70:/backup
-
-# Get the IP address of the machine assuming the network adapter is called enp2s0f1
 IP4=$(/sbin/ip -o -4 addr list $ETH0 | awk '{print $4}' | cut -d/ -f1)
-ZIP=/tmp/$IP4.zip
+SRC=/home/contestant
+DST=ioibckp@172.30.20.70:/backup/$IP4
 
-# Zip the home folder of the user
-sudo zip -r $ZIP $SRC
-
-# Next, rsync the zipped file to the backup server
-rsync -a -e ssh $ZIP $DST
+# Next, rsync the source folder to the backup server
+rsync -a -z $ZIP $DST
