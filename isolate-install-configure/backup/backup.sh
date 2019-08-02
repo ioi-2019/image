a6bfbf19fed3331f1755757783fd0dc6cc97caf9
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-08-02 17:33:28 +0400

    fix: mkdir added to backup.sh

--- isolate-install-configure/backup/backup.sh
+++ isolate-install-configure/backup/backup.sh
@@ -6,9 +6,13 @@
 
 # Variables
 ETH0=enp2s0f1
-IP4=$(/sbin/ip -o -4 addr list $ETH0 | awk '{print $4}' | cut -d/ -f1)
+IP=$(/sbin/ip -o -4 addr list $ETH0 | awk '{print $4}' | cut -d/ -f1)
 SRC=/home/contestant
-DST=ioibckp@172.30.20.70:/backup/$IP4
+SRV=ioibckp@172.30.20.70
+FOLDER=/backup/$IP
+DST=$SRV:$FOLDER
 
+# Make sure the backup directory is created
+ssh $SRV "mkdir -p $FOLDER"
 # Next, rsync the source folder to the backup server
 rsync -azlt --max-size=10m --exclude-from=/home/ansible/exclude $SRC $DST
