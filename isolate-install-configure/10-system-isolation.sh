535d3f205f3a17bd14f3a2ceaca91f0ed2e0c324
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-08-03 12:16:44 +0400

    fix: changed shell to bash for 10- script

--- isolate-install-configure/10-system-isolation.sh
+++ isolate-install-configure/10-system-isolation.sh
@@ -14,10 +14,10 @@ sudo systemctl disable ondemand
 
 #disable aslr:
 sudo touch /etc/sysctl.d/01-disable-aslr.conf
-sudo sh -c 'echo kernel.randomize_va_space = 0 >> /etc/sysctl.d/01-disable-aslr.conf'
+sudo bash -c 'echo kernel.randomize_va_space = 0 >> /etc/sysctl.d/01-disable-aslr.conf'
 
 #disable hugepage-related: 
 sudo apt -y install sysfsutils
-sudo sh -c 'echo kernel/mm/transparent_hugepage/enabled = never >> /etc/sysfs.conf'
-sudo sh -c 'echo kernel/mm/transparent_hugepage/defrag = never >> /etc/sysfs.conf'
-sudo sh -c 'echo kernel/mm/transparent_hugepage/khugepaged/defrag = 0 >> /etc/sysfs.conf'
+sudo bash -c 'echo kernel/mm/transparent_hugepage/enabled = never >> /etc/sysfs.conf'
+sudo bash -c 'echo kernel/mm/transparent_hugepage/defrag = never >> /etc/sysfs.conf'
+sudo bash -c 'echo kernel/mm/transparent_hugepage/khugepaged/defrag = 0 >> /etc/sysfs.conf'
